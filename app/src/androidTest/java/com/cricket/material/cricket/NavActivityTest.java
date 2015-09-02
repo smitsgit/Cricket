a7cdf6f8f6c6acb1fe0453bea62ae9ab731c221c
Smital Desai <desaismital@gmail.com>
2015-09-02 00:07:25 -0700

    Espresso Tests Written and Verified for Nav Drawer.

--- app/src/androidTest/java/com/cricket/material/cricket/NavActivityTest.java
+++ app/src/androidTest/java/com/cricket/material/cricket/NavActivityTest.java
@@ -1,6 +1,5 @@
 package com.cricket.material.cricket;
 
-import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -9,7 +8,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.contrib.DrawerActions.openDrawer;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
@@ -17,11 +17,28 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  */
 @RunWith(AndroidJUnit4.class)
 public class NavActivityTest {
+
     @Rule
     public ActivityTestRule<NavActivity> activityRule = new ActivityTestRule<>(NavActivity.class);
 
-    @Test
+   /* @Test
     public void shouldBeAbleToLaunchMainScreen(){
-        onView(withText("Hello")).check(ViewAssertions.matches(isDisplayed()));
+        onView(withText("Hello")).check(matches(isDisplayed()));
+    }*/
+
+    /**
+     * Test that clicking on a Navigation Drawer Item will open the correct fragment.
+     * Espresso: openDrawer, onView, withText, perform, click, matches, check, isDisplayed
+     */
+    @Test
+    public void testNavigationDrawerItemClick() {
+        openDrawer(R.id.drawer_layout);
+        onView(withText("Scores")).perform(click());
+    }
+
+
+    @Test
+    public void testNavigationDrawerOpen() {
+        openDrawer(R.id.drawer_layout);
     }
 }
